1d3aa30ae41479d6945c53ab8e7e3dff943cbbe3
Beatriz Zanardo <mbgzanardo@gmail.com>
2018-09-27 14:09:42 -0400

    correction to CART model

--- pa02/mzanardo-hw2-4.py
+++ pa02/mzanardo-hw2-4.py
@@ -4,7 +4,8 @@ from sklearn import tree
 import graphviz 
 
 
-data = [[0 for x in range(5)] for y in range(150)] 
+data = [[0 for x in range(4)] for y in range(150)] 
+labels = []
 
 with open("Dataset-film-data.csv") as f:
 	next(f)
@@ -14,31 +15,19 @@ with open("Dataset-film-data.csv") as f:
 		for value in line.split(","):
 			if i > 0:
 				if i == 5:
-					if value == "ACTION\n":
-						v = 0
-					if value == "COMEDY\n":
-						v = 1
-					if value == "ROMANCE\n":
-						v = 2
+					labels.append(value)
 				else:
-					v = value
-							
-				data[counter][i - 1] = v
+					data[counter][i - 1] = value
 
 			i +=1
 
 		counter += 1
 f.close()
 
-labels = []
-
-for j in range(0, 150):
-	labels.append(j)
-
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(data, labels)
 
-dot_data = tree.export_graphviz(clf, out_file=None, feature_names=["Ratings 1", "Ratings 2", "Ratings 3", "Ratings 4", "Genre"]) 
+dot_data = tree.export_graphviz(clf, out_file=None, feature_names=["Ratings 1", "Ratings 2", "Ratings 3", "Ratings 4"]) 
 graph = graphviz.Source(dot_data) 
 graph.render(view=True)
 
