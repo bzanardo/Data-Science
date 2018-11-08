99b2c5fca2327aa3fabaf8f8bfbd83d8aac2ed59
Beatriz Zanardo <mbgzanardo@gmail.com>
2018-11-08 16:51:08 -0500

    progress

--- pa04/mzanardo-hw4.py
+++ pa04/mzanardo-hw4.py
@@ -1,3 +1,5 @@
+import itertools as it
+
 
 def getSupport(item, db):
 	count = 0
@@ -17,6 +19,31 @@ with open("Dataset-apriori.txt") as f:
 		transaction_db[k] = []
 		for value in line.split()[1:]:
 			transaction_db[k].append(value)
+
+one_itemset = {}
+two_itemset = {}
+three_itemset = {}
+four_itemset = {}
+
+min_sup = 2
+items = ['a', 'b', 'c', 'd', 'e']
+
+for i in items:
+	sup = getSupport([i], transaction_db)
+	if sup >= min_sup:
+		one_itemset[i] = sup
+
+two_comb = list(it.combinations(one_itemset.keys(), 2))
+
+for i in two_comb:
+	sup = getSupport(list(i), transaction_db)
+	if sup >= min_sup:
+		key = str(i[0]) + str(i[1])
+		two_itemset[key] = sup
+
+print(two_itemset)
+
+
 
 
 
