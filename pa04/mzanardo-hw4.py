7928663b42b6f4a08c8991b040c6a43f5253ac2a
Beatriz Zanardo <mbgzanardo@gmail.com>
2018-11-09 15:00:41 -0500

    three itemset

--- pa04/mzanardo-hw4.py
+++ pa04/mzanardo-hw4.py
@@ -9,9 +9,32 @@ def getSupport(item, db):
 
 	return count
 
+def findCandidates(db, lenght):
+	index = lenght - 2
+	candidates = {}
+	keys = list(db.keys())
+	current_item = keys[0]
+	count = 0
+
+
+	for k, v in db.items():
+		#print(k)
+		print(k[:index])
+		print(current_item[:index])
+
+		if k == current_item:
+	 		continue
+
+		if k[:index] == current_item[:index]:
+			new_item = current_item + k[-1]
+			candidates[new_item] = list(it.combinations(list(new_item), lenght))#[current_item, k, current_item[1]+ k[1]]
+		else:
+			current_item = k
+
+	return(candidates)
+
 
 transaction_db = {}
-
 with open("Dataset-apriori.txt") as f:
 	next(f)
 	for line in f.readlines():
@@ -41,7 +64,44 @@ for i in two_comb:
 		key = str(i[0]) + str(i[1])
 		two_itemset[key] = sup
 
+#candidates = {}
+#current_item = list(two_itemset.keys())[0]
+
 print(two_itemset)
+candidates = findCandidates(two_itemset, 3)
+print(candidates)
+
+# for k, v in two_itemset.items():
+# 	if k == current_item:
+# 		continue
+# 	if k[0] == current_item[0]:
+# 		new_item = current_item + k[1:]
+# 		candidates[new_item] = [current_item, k, current_item[1]+ k[1]]
+# 	else:
+# 		current_item = k
+
+
+# for k, v in candidates.items():
+# 	valid = True
+# 	for i in v:
+# 		if i not in two_itemset:
+# 			valid = False
+# 			break 
+# 		else:
+# 			sup = two_itemset[str(i)]
+
+# 		if sup < min_sup:
+# 			valid = False
+# 			break
+
+# 	if valid:
+# 		supp = getSupport(list(k), transaction_db)
+# 		if supp >= min_sup:
+# 			three_itemset[k] = supp
+
+
+
+
 
 
 
